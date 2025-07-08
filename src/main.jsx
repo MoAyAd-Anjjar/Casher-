a62dfc3aca78393aa09006c3ef569831346cf9b5
Moayad anjjar <anjjarmoayad@gmail.com>
2025-07-08 19:44:41 +0300

    fix app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import DataProvider from "./Provider/DataProvider.js";
+import DataProvider from "./Provider/DataProvider";
 import { ToastContainer } from "react-toastify";
+import App from "./App"
 createRoot(document.getElementById("root")).render(
   <DataProvider>
     <ToastContainer
